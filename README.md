ebda6aeaffc4d1c50289a4068a53938ef2b7cde0
Felipe Canales <felipe.canales.213@gmail.com>
2022-01-07 19:21:18 -0300

    documentation

---
+++ README.md
@@ -0,0 +1,16 @@
+# PokeAPI exercise
+
+A small excercise of 3 questions using [PokeAPI](https://pokeapi.co).
+There is no error handling, as the impossibility to get an answer should be handled by the code calling these functions.
+
+
+### Answers
+
++ Question 1: 9
++ Question 2: 258
++ Question 3: [1300, 195]
+
+### Requirements
+
++ python 3.8.10
++ requests 2.27.1
--- questions.py
+++ questions.py
@@ -2,11 +2,21 @@ import requests
 
 
 def question_1():
+    """Indicates how many pokemons have 'at' and exactly two 'a's in their names.
+
+    Returns:
+        int: amount of pokemon satifying the condition
+    """    
     all_pokemon = requests.get('https://pokeapi.co/api/v2/pokemon-species/?limit=2000').json() #get all pokemon in one request
     return len([pokemon['name'] for pokemon in all_pokemon['results'] if ('at' in pokemon['name'] and pokemon['name'].count('a') == 2)])
 
 
 def question_2(): 
+    """Indicates how many pokemon can breed with Raichu.
+
+    Returns:
+        int: amount of pokemon satifying the condition
+    """    
     SPECIES_QUERY = 'raichu'
     species_set = set()
     species_info = requests.get('https://pokeapi.co/api/v2/pokemon-species/{}'.format(SPECIES_QUERY)).json()
@@ -19,8 +29,15 @@ def question_2():
 
 
 def question_3():
+    """Indicates the maximum and minimum weights for gen 1 pokemon.
+
+    Returns:
+        list(int): max and min values
+    """    
     GEN = 1
     TYPE = 'fighting'
+    # Candidate pokemons are filtered by generation.
+    # Assumption: an extra request is less efficient than an 'in' operation
     gen_pkmns = requests.get('https://pokeapi.co/api/v2/generation/{}'.format(GEN)).json()['pokemon_species']
     type_pkmns = requests.get('https://pokeapi.co/api/v2/type/{}'.format(TYPE)).json()['pokemon']
     gen_pkmn_names = [pokemon['name'] for pokemon in gen_pkmns]
@@ -32,7 +49,7 @@ def question_3():
         pokemon_weight = requests.get(pokemon['pokemon']['url']).json()['weight']
         if pokemon_weight > max_weigth:
             max_weigth = pokemon_weight
-        elif pokemon_weight < min_weight:
+        if pokemon_weight < min_weight:
             min_weight = pokemon_weight
 
     return [max_weigth, min_weight]

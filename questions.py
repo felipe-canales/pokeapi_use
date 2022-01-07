dabca7ca0a157cc0fe1f3c3a4c6b7a55689769a6
Felipe Canales <felipe.canales.213@gmail.com>
2022-01-07 18:02:47 -0300

    alternative for question 2

--- questions.py
+++ questions.py
@@ -1,4 +1,6 @@
-import pokebase
+#import pokebase
+import requests
+
 
 def question_2():
     SPECIES_QUERY = 'raichu'
@@ -10,5 +12,17 @@ def question_2():
     
     return len(species_set)
 
+def question_2_alt():
+    SPECIES_QUERY = 'raichu'
+    species_set = set()
+    species_info = requests.get('https://pokeapi.co/api/v2/pokemon-species/{}'.format(SPECIES_QUERY)).json()
+    for egg_group in species_info['egg_groups']:
+        egg_group_info = requests.get(egg_group['url']).json()
+        for egg_group_species in egg_group_info['pokemon_species']:
+            species_set.add(egg_group_species['name'])
+    
+    return len(species_set)
+
+
 if __name__ == '__main__':
-    print(question_2())
+    print(question_2_alt())

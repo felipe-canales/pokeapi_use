80694422d02b31f43dee147540a538c8d93baf3d
Felipe Canales <felipe.canales.213@gmail.com>
2022-01-07 19:02:36 -0300

    question 3

--- questions.py
+++ questions.py
@@ -2,11 +2,11 @@ import requests
 
 
 def question_1():
-    all_pokemon = requests.get('https://pokeapi.co/api/v2/pokemon-species/?limit=2000').json() #all pokemon in one request
+    all_pokemon = requests.get('https://pokeapi.co/api/v2/pokemon-species/?limit=2000').json() #get all pokemon in one request
     return len([pokemon['name'] for pokemon in all_pokemon['results'] if ('at' in pokemon['name'] and pokemon['name'].count('a') == 2)])
 
 
-def question_2():
+def question_2(): 
     SPECIES_QUERY = 'raichu'
     species_set = set()
     species_info = requests.get('https://pokeapi.co/api/v2/pokemon-species/{}'.format(SPECIES_QUERY)).json()
@@ -19,9 +19,26 @@ def question_2():
 
 
 def question_3():
-    pass
+    GEN = 1
+    TYPE = 'fighting'
+    gen_pkmns = requests.get('https://pokeapi.co/api/v2/generation/{}'.format(GEN)).json()['pokemon_species']
+    type_pkmns = requests.get('https://pokeapi.co/api/v2/type/{}'.format(TYPE)).json()['pokemon']
+    gen_pkmn_names = [pokemon['name'] for pokemon in gen_pkmns]
+    max_weigth = -1
+    min_weight = 999999
+    for pokemon in type_pkmns:
+        if pokemon['pokemon']['name'] not in gen_pkmn_names:
+            continue
+        pokemon_weight = requests.get(pokemon['pokemon']['url']).json()['weight']
+        if pokemon_weight > max_weigth:
+            max_weigth = pokemon_weight
+        elif pokemon_weight < min_weight:
+            min_weight = pokemon_weight
 
+    return [max_weigth, min_weight]
 
 
 if __name__ == '__main__':
     print(question_1())
+    print(question_2())
+    print(question_3())
